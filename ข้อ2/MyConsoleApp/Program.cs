9de02ad6dae7bf72fe8a76e28f900522546f559d
HK09x <89598486+HK09x@users.noreply.github.com>
2024-07-05 02:43:10 +0000

    .

---
+++ ข้อ2/MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 10;
+        int zero = 0;
+        int result = number / zero; // This will cause a runtime error (DivideByZeroException)
+        Console.WriteLine(result);
+    }
+}
